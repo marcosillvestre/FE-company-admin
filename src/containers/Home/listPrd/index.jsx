c55dc1265b96c734636a6959fa8047d5a0bac1d3
marcos vinicius <marcos.vinicius7170@gmail.com>
2022-12-28 22:22:12 -0300

    releases list/home

--- src/containers/Home/listPrd/index.jsx
+++ src/containers/Home/listPrd/index.jsx
@@ -5,10 +5,12 @@ import formatDate from '../../../services/formateDate'
 import { Carrousel, Container, ContainerItens, MachineNav, Production } from './styles'
 
 export function ListProd() {
+
     const { location: { state } } = useHistory()
     console.log(state)
 
     let categoryId = 0
+
 
 
 
@@ -25,87 +27,87 @@ export function ListProd() {
 
         }
 
-        async function getAllMachines() {
-            const { data } = await fetchapi.get('todas-maquinas')
-            setRepetitive(data)
+        //     async function getAllMachines() {
+        //         const { data } = await fetchapi.get('todas-maquinas')
+        //         setRepetitive(data)
 
-        }
+        //     }
 
-        getAllMachines()
-        getProd()
+        //     getAllMachines()
+        //     getProd()
 
-    }, [activeMachine])
-
-
-    console.log(filtered)
-    // usar o activeMachine para setar a produção
-
-    useEffect(() => {
-
-        if (activeMachine === 0) {
-            setFiltered(data)
-        } else {
-            const final = filtered.filter(
-                infos => infos.machine_id === activeMachine
-            )
-            setFiltered(final)
-        }
-
-    }, [activeMachine, data, filtered])
+        // }, [activeMachine])
 
 
+        console.log(filtered)
+        // usar o activeMachine para setar a produção
 
+        useEffect(() => {
 
-    return (
-        <Container >
-            <MachineNav>
-                {
-                    repetitive && repetitive.map(machine => (
-                        <button
-                            onClick={() => setActiveMachine(machine.id)}
-                            key={machine.id}>
-                            {machine.machine}
-                        </button>
+            if (activeMachine === 0) {
+                setFiltered(data)
+            } else {
+                const final = filtered.filter(
+                    infos => infos.machine_id === activeMachine
+                )
+                setFiltered(final)
+            }
 
-                    ))
-                }
-            </MachineNav>
-
-
-            <ContainerItens>
-
-                <Carrousel>
-                    <div>Produção/hora</div>
-                    <div>Refugos</div>
-                    <div>Operador</div>
-                    <div>Lançado às</div>
-                    <div>Turno</div>
-                </Carrousel>
-                {
-                    filtered && filtered.map(info => (
-
-
-                        <Production key={info.id}>
-                            <div>{info.prod_per_hour}</div>
-                            <div>{info.lost_prod}</div>
-                            <div>{info.operator}</div>
-                            <div>{formatDate(info.updatedAt)}</div>
-                            <div>{ }</div>
-                        </Production>
-                    ))
-                }
+        }, [activeMachine, data, filtered])
 
 
 
 
+        return (
+            <Container >
+                <MachineNav>
+                    {
+                        repetitive && repetitive.map(machine => (
+                            <button
+                                onClick={() => setActiveMachine(machine.id)}
+                                key={machine.id}>
+                                {machine.machine}
+                            </button>
+
+                        ))
+                    }
+                </MachineNav>
 
 
-            </ContainerItens>
+                <ContainerItens>
+
+                    <Carrousel>
+                        <div>Produção/hora</div>
+                        <div>Refugos</div>
+                        <div>Operador</div>
+                        <div>Lançado às</div>
+                        <div>Turno</div>
+                    </Carrousel>
+                    {
+                        filtered && filtered.map(info => (
+
+
+                            <Production key={info.id}>
+                                <div>{info.prod_per_hour}</div>
+                                <div>{info.lost_prod}</div>
+                                <div>{info.operator}</div>
+                                <div>{formatDate(info.updatedAt)}</div>
+                                <div>{ }</div>
+                            </Production>
+                        ))
+                    }
 
 
 
-        </Container>)
-}
+
+
+
+                </ContainerItens>
+
+
+
+            </Container>)
+    }
 
 export default ListProd
 
